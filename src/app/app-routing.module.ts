4160cfb4eb14d8974c0777516f7955539f6d0ba2
mariem548 <mariembensalah131@gmail.com>
2021-07-05 12:22:32 +0200

    ajout acceuil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'acceuil',pathMatch:'full'},
+
+  {path:'acceuil', component:AcceuilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnexionComponent
+    ConnexionComponent,
+    AcceuilComponent
   ],
   imports: [
     BrowserModule,
