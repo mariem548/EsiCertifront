0b665113386f6b02e59fa6249186af5b37add317
Astr0TV <lamsaouriismail@gmail.com>
2021-07-06 11:58:27 +0200

    Merge branch 'main' of https://github.com/mariem548/EsiCertifront

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'acceuil',pathMatch:'full'},
 
-  {path:'acceuil', component:AcceuilComponent}
+  {path:'acceuil', component:AcceuilComponent},
+  {path:'connexion', component: ConnexionComponent}
 ];
 
 @NgModule({
